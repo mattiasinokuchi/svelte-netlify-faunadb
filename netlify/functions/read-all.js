76852a63356338b475f537e1e0dbbc9e8f38bad9
mattiasinokuchi <8-b@tuta.io>
2021-04-13 13:18:45 +0200

    fixed read-all using example for Vercel

--- netlify/functions/read-all.js
+++ netlify/functions/read-all.js
@@ -3,33 +3,28 @@ const faunadb = require('faunadb')
 const q = faunadb.query
 
 
-exports.handler = (event, context) => {
-  console.log('Function `todo-read-all` invoked')
-  /* configure faunaDB Client with our secret */
-  const client = new faunadb.Client({
-    secret: process.env.FAUNADB_SERVER_SECRET
-  }) 
-  return client.query(q.Paginate(q.Match(q.Collection('test'))))
-    .then((response) => {
-      const todoRefs = response.data
-      console.log('Todo refs', todoRefs)
-      console.log(`${todoRefs.length} todos found`)
-      // create new query out of todo refs. http://bit.ly/2LG3MLg
-      const getAllTodoDataQuery = todoRefs.map((ref) => {
-        return q.Get(ref)
-      })
-      // then query the refs
-      return client.query(getAllTodoDataQuery).then((ret) => {
-        return {
-          statusCode: 200,
-          body: JSON.stringify(ret)
-        }
-      })
-    }).catch((error) => {
-      console.log('error', error)
+exports.handler = async (event, context) => {
+  try {
+    console.log('Function `read-all` invoked')
+    const client = new faunadb.Client({
+      secret: process.env.FAUNADB_SERVER_SECRET
+    });
+    let collections = []; 
+    await client
+      .paginate(q.Collections())
+      .map(ref => q.Get(ref))
+      .each(page => {
+        collections = collections.concat(page);
+      });
       return {
-        statusCode: 400,
-        body: JSON.stringify(error)
+        statusCode: 200,
+        body: JSON.stringify(collections)
       }
-    })
+  } catch(error) {
+    console.log(error);
+    return {
+      statusCode: 400,
+      body: JSON.stringify(error)
+    }
+  }
 }
