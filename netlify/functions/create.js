33eb9f49c5140c79197a08d63604a78ee5a6476c
mattiasinokuchi <8-b@tuta.io>
2021-04-13 13:42:21 +0200

    changed to await

--- netlify/functions/create.js
+++ netlify/functions/create.js
@@ -2,34 +2,30 @@ const faunadb = require('faunadb')
 const q = faunadb.query
 
 exports.handler = async (event, context) => {
-  const client = new faunadb.Client({
-    secret: process.env.FAUNADB_SERVER_SECRET
-  })
+  try {
+    const client = new faunadb.Client({
+      secret: process.env.FAUNADB_SERVER_SECRET
+    })
 
-  console.log('Create test function invoked');
+    console.log('Function `create` invoked');
 
-  const createP = client.query(
-    q.Create(
-      q.Collection('test'),
-      { data: { testField: 'testValue' } }
-    )
-  )
+    const response = await client
+      .query(
+        q.Create(
+          q.Collection('test'), { data: { testField: 'testValue' } }
+        )
+      )
 
-  return createP
-    .then(
-      function (response) {
-        console.log(response.ref);
-        return {
-          statusCode: 200,
-          body: JSON.stringify(response)
-        }
-      }
-    )
-    .catch((error) => {
-      console.log('error', error)
-      return {
-        statusCode: 400,
-        body: JSON.stringify(error)
-      }
-    });
+    return {
+      statusCode: 200,
+      body: JSON.stringify(response)
+    }
+
+  } catch (error) {
+    console.log(error)
+    return {
+      statusCode: 400,
+      body: JSON.stringify(error)
+    }
+  }
 }
