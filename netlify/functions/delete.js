8332c0e14f9fe64499314c59911d3c7892036179
mattiasinokuchi <8-b@tuta.io>
2021-04-19 19:02:08 +0200

    added delete function

---
+++ netlify/functions/delete.js
@@ -0,0 +1,25 @@
+const faunadb = require('faunadb');
+const q = faunadb.query;
+
+
+exports.handler = async (event, context) => {
+  try {
+    console.log('Function `delete` invoked');
+    const client = new faunadb.Client({
+      secret: process.env.FAUNADB_SERVER_SECRET
+    });
+    const id = JSON.parse(event.body);
+    // delete document
+    const response = await client.query(q.Delete(q.Ref(`classes/todos/${id}`)))
+    return {
+      statusCode: 200,
+      body: JSON.stringify(response)
+    }
+  } catch (error) {
+    console.log('error', error)
+    return {
+      statusCode: 400,
+      body: JSON.stringify(error)
+    }
+  }
+}
