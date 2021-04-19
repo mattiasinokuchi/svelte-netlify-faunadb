43ee97ad4db062dc7c497573e0c54c3d43502561
mattiasinokuchi <8-b@tuta.io>
2021-04-19 18:48:39 +0200

    renamed constants

--- netlify/functions/read-all.js
+++ netlify/functions/read-all.js
@@ -7,21 +7,20 @@ exports.handler = async (event, context) => {
     const client = new faunadb.Client({
       secret: process.env.FAUNADB_SERVER_SECRET
     });
-    const response = await client
+    // Get an object with an array of all documents Ref ID
+    const refObj = await client
       .query(q.Paginate(q.Match(q.Ref("indexes/todos"))));
-    console.log(response);
-    const todoRefs = response.data
-    console.log("Todo refs", todoRefs)
-    console.log(`${todoRefs.length} todos found`)
-    const getAllTodoDataQuery = await todoRefs.map((ref) => {
+    // Get array of all documents Ref ID
+    const refArr = refObj.data;
+    // Create a query for all data in documents 
+    const dataQuery = await refArr.map((ref) => {
       return q.Get(ref)
     });
-    console.log(getAllTodoDataQuery);
-    const ret = await client.query(getAllTodoDataQuery);
-    console.log(ret);
+    // Get all data in documents
+    const dataArr = await client.query(dataQuery);
     return {
       statusCode: 200,
-      body: JSON.stringify(ret)
+      body: JSON.stringify(dataArr)
     }
   } catch (error) {
     console.log(error);
