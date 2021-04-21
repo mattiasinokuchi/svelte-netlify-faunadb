cea21654417b7023b4706052e9fe6b513a78dcca
mattiasinokuchi <8-b@tuta.io>
2021-04-21 13:54:00 +0200

    added update function

---
+++ netlify/functions/update.js
@@ -0,0 +1,39 @@
+const faunadb = require('faunadb')
+const q = faunadb.query
+
+exports.handler = async (event, context) => {
+  try {
+    console.log('Function `update` invoked');
+    const client = new faunadb.Client({
+      secret: process.env.FAUNADB_SERVER_SECRET
+    });
+    const id = JSON.parse(event.body.id);
+    const data = JSON.parse(event.body.data);
+    await client.query(
+      q.Update(
+        q.Ref(`classes/todos/${id}`), { data }
+      )
+    );
+    // Get an object with an array of all documents Ref ID
+    const refObj = await client
+      .query(q.Paginate(q.Match(q.Ref("indexes/todos"))));
+    // Get array of all documents Ref ID
+    const refArr = refObj.data;
+    // Create a query for all data in documents 
+    const dataQuery = await refArr.map((ref) => {
+      return q.Get(ref)
+    });
+    // Get all data in documents
+    const dataArr = await client.query(dataQuery);
+    return {
+      statusCode: 200,
+      body: JSON.stringify(dataArr)
+    }
+  } catch (error) {
+    console.log(error)
+    return {
+      statusCode: 400,
+      body: JSON.stringify(error)
+    }
+  }
+}
--- src/Main.svelte
+++ src/Main.svelte
@@ -4,17 +4,35 @@
 
     let i = 0;
 
-    const select = num => function() {
-        console.log(num);
-        console.log($todos[num-1]);
-        console.log($todos[num-1]['ref']['@ref']['id']);
-    }
-
     onMount(async () => {
         const response = await fetch("/.netlify/functions/read-all");
         $todos = await response.json();
         console.log("Array of todos", $todos);
     });
+
+    const update = (num) =>
+        async function () {
+            try {
+                console.log(num);
+                console.log($todos[num - 1]);
+                console.log($todos[num - 1]["ref"]["@ref"]["id"]);
+                const response = await fetch("/.netlify/functions/update", {
+                    method: "POST",
+                    body: JSON.stringify({
+                        id: $todos[num - 1]["ref"]["@ref"]["id"],
+                        data: $todos[num - 1]["data"]["name"],
+                    }),
+                    headers: {
+                        "Content-Type": "application/json",
+                    },
+                });
+                $todos = await response.json();
+                console.log($todos);
+                newTodo = "";
+            } catch (error) {
+                console.log(error);
+            }
+        };
 </script>
 
 <main>
@@ -22,10 +40,7 @@
     {#if $todos}
         <h2>
             {#each $todos as { data }, i}
-                <input
-                    bind:value={data.name}
-                    on:focus={select(i + 1)}
-                />
+                <input bind:value={data.name} on:change={update(i + 1)} />
             {/each}
         </h2>
     {/if}
