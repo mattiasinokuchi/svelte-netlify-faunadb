2f14bf4708158d4be37daf914a12629e4bc016cb
mattiasinokuchi <8-b@tuta.io>
2021-04-21 15:58:51 +0200

    slammed that delete function!!!

--- netlify/functions/delete.js
+++ netlify/functions/delete.js
@@ -7,12 +7,26 @@ exports.handler = async (event, context) => {
     const client = new faunadb.Client({
       secret: process.env.FAUNADB_SERVER_SECRET
     });
-    const id = JSON.parse(event.body);
-    // delete document
-    const response = await client.query(q.Delete(q.Ref(`classes/todos/${id}`)))
+    const object = JSON.parse(event.body);
+    await client.query(
+      q.Delete(
+        q.Ref(`classes/todos/${object.id}`)
+      )
+    );
+    // Get an object with an array of all documents Ref ID
+    const refObj = await client
+      .query(q.Paginate(q.Match(q.Ref("indexes/todos"))));
+    // Get array of all documents Ref ID
+    const refArr = refObj.data;
+    // Create a query for all data in documents 
+    const dataQuery = await refArr.map((ref) => {
+      return q.Get(ref)
+    });
+    // Get all data in documents
+    const dataArr = await client.query(dataQuery);
     return {
       statusCode: 200,
-      body: JSON.stringify(response)
+      body: JSON.stringify(dataArr)
     }
   } catch (error) {
     console.log('error', error)
--- src/Main.svelte
+++ src/Main.svelte
@@ -32,6 +32,29 @@
                 console.log(error);
             }
         };
+
+    const remove = (num) =>
+        async function () {
+            try {
+                console.log(num);
+                console.log($todos[num - 1]);
+                console.log($todos[num - 1]["ref"]["@ref"]["id"]);
+                const response = await fetch("/.netlify/functions/delete", {
+                    method: "POST",
+                    body: JSON.stringify({
+                        id: $todos[num - 1]["ref"]["@ref"]["id"],
+                        data: $todos[num - 1]["data"]["name"],
+                    }),
+                    headers: {
+                        "Content-Type": "application/json",
+                    },
+                });
+                $todos = await response.json();
+                console.log($todos);
+            } catch (error) {
+                console.log(error);
+            }
+        };
 </script>
 
 <main>
@@ -40,7 +63,7 @@
         <h2>
             {#each $todos as { data }, i}
                 <div id="todo">
-                    <button class="button">🗑</button>
+                    <button class="button" on:click={remove(i + 1)}>🗑</button>
                     <input bind:value={data.name} on:change={update(i + 1)} />
                 </div>
             {/each}
