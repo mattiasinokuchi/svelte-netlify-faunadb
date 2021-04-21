a020551e99cd496fa52e73345577a1f26b57144a
Mattias Inokuchi <46524414+mattiasinokuchi@users.noreply.github.com>
2021-04-21 06:19:07 +0200

    started on update function

--- src/Main.svelte
+++ src/Main.svelte
@@ -2,6 +2,14 @@
     import { onMount } from "svelte";
     import { todos } from "./stores";
 
+    let i = 0;
+
+    const select = num => function() {
+        console.log(num);
+        console.log($todos[num-1]);
+        console.log($todos[num-1]['ref']['@ref']['id']);
+    }
+
     onMount(async () => {
         const response = await fetch("/.netlify/functions/read-all");
         $todos = await response.json();
@@ -13,8 +21,11 @@
     <h1>To do</h1>
     {#if $todos}
         <h2>
-            {#each $todos.reverse() as { data }}
-                <input bind:value={data.name} />
+            {#each $todos as { data }, i}
+                <input
+                    bind:value={data.name}
+                    on:focus={select(i + 1)}
+                />
             {/each}
         </h2>
     {/if}
