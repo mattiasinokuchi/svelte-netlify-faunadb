9108d51b196cef1ccfa70d712a8cf067c1e3c4e6
mattiasinokuchi <8-b@tuta.io>
2021-05-10 12:41:31 +0200

    added test file

---
+++ src/List.spec.js
@@ -0,0 +1,10 @@
+import { cleanup, fireEvent, render, waitFor } from '@testing-library/svelte';
+import List from './List.svelte';
+
+describe('List', () => {
+    afterEach(cleanup);
+    test('should render', async () => {
+        const { getByText } = render(List);
+        expect(getByText('To Do List'));
+    });
+});
